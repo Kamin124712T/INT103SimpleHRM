12ff4a2975f0381b894daa4e216699153b30d925
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-02-20 15:32:10 +0700

    SimpleHRM Lab error

--- src/main/java/simplehrm01/main/SimpleHRM.java
+++ src/main/java/simplehrm01/main/SimpleHRM.java
@@ -5,6 +5,7 @@ import simplehrm01.model.Faculty;
 import simplehrm01.model.Graduated;
 import simplehrm01.model.Person;
 import simplehrm01.model.Staff;
+import simplehrm01.model.Student;
 import simplehrm01.model.Undergraduate;
 
 public class SimpleHRM {
@@ -21,7 +22,27 @@ public class SimpleHRM {
         person[5] = new Faculty("Professor Dr. ", "Vinai Sukum", "sukum.v@sit.kmutt.ac.th");
         person[6] = new Graduated(Graduated.programFlag.weekdays, "585238759", "Manasuk Katoke");
 
+        //1
+        Person person01 = new Staff("HRM", "Ana Jo", "my@sit.com");
+        //2
+        //Person person02 = new Employee("SIT building",25000);
+        //3
+        Faculty person03 = new Faculty("Prapakeaw Aristawan","fac@sit.kmutt.com");
         Arrays.sort(person);
+        //4
+        //Student std01 = new Student();
+        //5
+        Undergraduate std02 = new Undergraduate("582272667","Yutta Ussamalaw");
+        //6
+        //Person std03 = new Graduated();
+        //7
+        Student p002 = (Student)person[0];
+        //8
+        Student p003 = (Graduated)person[6];
+        //9
+        //Person p004 = (Employee)person[3];
+        //10
+        Graduated p005 = (Graduated)person[0];
 
         for (int i = 0; i < person.length; i++) {
             System.out.println(person[i].toString());
